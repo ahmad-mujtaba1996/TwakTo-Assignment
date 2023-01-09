4b916bbb3af917e96d36546ff0569496a1b8daa9
Ahmad Mujtaba <ahmad.mujtaba@jazz.com.pk>
2023-01-09 11:46:45 +0000

    Initial Commit

---
+++ TwakToTestAssignment/TwakToTestAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwakToTestAssignment
+//
+//  Created by Syed Ahmad Mujtaba on 09/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
